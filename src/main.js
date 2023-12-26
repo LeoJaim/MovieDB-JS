f34c849559a59ff796003883b685ca9971e2eb74
Leojaim <leonardo.jaimovich@gmail.com>
2023-12-26 15:49:57 -0300

    axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headres:{
+        'Content-Type': 'application/json;characterset=utf=8',
+    },
+    params:{
+        'api_key':API_KEY,
+    },
+});
+
 async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await res.json();
+    const { data } = await api('trending/movie/day');
     const movies = data.results;
+
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector
         ('#trendingPreview .trendingPreview-movieList');
@@ -25,8 +35,7 @@ async function getTrendingMoviesPreview() {
 }
 
 async function getCategories() {
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-    const data = await res.json();
+    const {data} = await api('genre/movie/list');
     const categories = data.genres; //el data. varía segun la definición de la API , aca es data.genres. En movies es data.results
     
     categories.forEach(category => {
